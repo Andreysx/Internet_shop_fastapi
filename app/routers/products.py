2b492904d25b6886f29feadcf7cb0e0c3640d623
Andrey <89645083593dron@gmail.com>
2026-01-30 01:45:29 +0300

    Create get and post enpoints

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,24 +1,50 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy import select, update
+from sqlalchemy.orm import Session
+
+from app.models.products import Product as ProductModel
+from app.models.categories import Category as CategoryModel
+from app.schemas import Product as ProductSchema, ProductCreate as ProductCreateSchema
+from app.db_depends import get_db
+
+
+
+
 #from fastapi import APIRouter – класс для создания маршрутизатора.
 
 # Создаем маршрутизатор для товаров
 router = APIRouter(prefix="/products", tags=["products"])
 
 
-@router.get(path="/")
-async def get_all_products():
+@router.get(path="/", response_model=list[ProductSchema], status_code=status.HTTP_200_OK)
+async def get_all_products(db: Session = Depends(get_db)):
     """
     Возвращает список всех товаров
     """
-    return {"message": "Список всех товаров (заглушка)"}
+    stmt = select(ProductModel).where(ProductModel.is_active == True)
+    products = db.scalars(stmt).all()
+
+    return products
 
 
-@router.post(path="/")
-async def create_product():
+@router.post(path="/", response_model=ProductSchema, status_code=status.HTTP_201_CREATED)
+async def create_product(product: ProductCreateSchema, db: Session = Depends(get_db)):
     """
     Создаёт новый товар
     """
-    return {"message": "Товар создан (заглушка)"}
+    #Проверка существования категории
+    if product.category_id is not None:
+        stmt = select(CategoryModel).where(CategoryModel.id == product.category_id, CategoryModel.is_active == True)
+        category = db.scalars(stmt).first()
+        if category is None:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Category not found")
+
+    #Создание нового продукта в БД
+    db_product = ProductModel(**product.model_dump())
+    db.add(db_product)
+    db.commit()
+    db.refresh(db_product)
+    return db_product
 
 
 @router.get(path="/category/{category_id}")
