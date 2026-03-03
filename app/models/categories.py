9e5a145d4b94225b1393762c7a733d73352bc201
Andrey <89645083593dron@gmail.com>
2026-03-03 18:51:13 +0300

    docs/style(models): deleted unnecessary comments, left important

--- app/models/categories.py
+++ app/models/categories.py
@@ -1,9 +1,5 @@
 from sqlalchemy import String, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-# Mapped, mapped_column Инструменты SQLAlchemy 2.x для строгой типизации.
-# современный синтаксис mapped_column из SQLAlchemy 2.x, который обеспечивает строгую типизацию и улучшенную читаемость
-# relationship, ForeignKey: Используются для настройки связей.
-# Атрибуты products, parent, и children не влияют на SQL-схему, так как relationship работает на уровне Python.
 
 from app.database import Base
 
@@ -18,22 +14,6 @@ class Category(Base):
 
     products: Mapped[list["Product"]] = relationship("Product", back_populates="category")
 
-    #   Реализация самоссылающейся таблицы(самоссылающуюся связь) в sqlalchemy
-    #   Самоссылающаяся таблица - это таблица, которая ссылается на себя через внешний ключ (ForeignKey)
-    #   Позволяет создавать иерархию одной и той же сущности для гибкости
     parent: Mapped["Category | None"] = relationship("Category", back_populates="children", remote_side="Category.id")
 
     children: Mapped[list["Category"]] = relationship("Category", back_populates="parent")
-
-# Модели соответствуют Pydantic-моделям из файла app/schemas.py, что позволит легко преобразовывать данные между API и базой данных.
-# Модели данных определяют структуру таблиц в БД
-# Модульная структура (app/models) делает проект организованным, а проверка SQL-кода помогает убедиться в корректности моделей перед созданием таблиц.
-
-#
-# if __name__ == "__main__":
-#     # Проверка SQL-кода, который SQLAlchemy генерирует для создания таблиц
-#     from sqlalchemy.schema import CreateTable
-#     from app.models.products import Product
-#
-#     print(CreateTable(Category.__table__))
-#     print(CreateTable(Product.__table__))
