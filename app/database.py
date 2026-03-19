a3c87915c64cd21e24c3cc95c42b9dac9675bfa0
Andrey <89645083593dron@gmail.com>
2026-03-20 00:08:45 +0300

    build(docker): added Dockerfile and docker-compose.yml for containerized local deployment

--- app/database.py
+++ app/database.py
@@ -18,7 +18,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from sqlalchemy.orm import DeclarativeBase
 
 # Строка подключения для PostgreSQl
-DATABASE_URL = "postgresql+asyncpg://ecommerce_user:12345@localhost:5432/ecommerce_db"
+DATABASE_URL = "postgresql+asyncpg://ecommerce_user:xxxxxxxx@db:5432/ecommerce_db"
 
 async_engine = create_async_engine(DATABASE_URL, echo=True)
 
